a40f2e045c33e7fc8f51a96d54a7b7a06a6f84f1
Louisjreeves <79279019+Louisjreeves@users.noreply.github.com>
2025-05-14 23:09:14 -0500

    Add files via upload

---
+++ RunHelp.ps1
@@ -0,0 +1,40 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Get Downloads folder path
+$DownloadsPath = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+Set-Location -Path $DownloadsPath
+
+# Define file and paths
+$zipName = "AZURE.LOCAL.INFO.CAPTURES.Session(ALICS)2.24.zip"
+$folderName = "AZURE.LOCAL.INFO.CAPTURES.Session(ALICS)2.24"
+#$downloadUrl = "https://github.com/Louisjreeves/WinClusArcHelper/raw/refs/heads/main/$zipName"
+$downloadUrl = "https://github.com/Louisjreeves/WinClusArcHelper/raw/refs/heads/main/AZURE.LOCAL.INFO.CAPTURES.Session(ALICS)2.24.zip"
+$zipPath = Join-Path $DownloadsPath $zipName
+$extractPath = Join-Path $DownloadsPath $folderName
+
+# Download ZIP
+try {
+    Invoke-WebRequest -Uri $downloadUrl -OutFile $zipPath -UseBasicParsing -ErrorAction Stop
+} catch {
+    Write-Error "Download failed: $($_.Exception.Message)"
+    exit 1
+}
+
+# Extract it
+try {
+    Expand-Archive -Path $zipPath -DestinationPath $extractPath -Force
+} catch {
+    Write-Error "Failed to unzip $zipName"
+    exit 1
+}
+
+ $scriptFileName = "AZURE.LOCAL.INFO.CAPTURES.Session(ALICS)2.24.ps1"
+$scriptPath = Join-Path $extractPath $scriptFileName
+
+if (Test-Path $scriptPath) {
+    Set-Location -Path $extractPath
+    & $scriptPath
+} else {
+    Write-Error "Script not found: $scriptPath"
+    exit 1
+}
